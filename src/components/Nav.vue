7c23e196719f8fd1cdf30b3fdb288fff6fbfba90
Soham Tamboli <sohamtamboli@gmail.com>
2020-10-01 18:58:37 +0530

    initial project grid

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -15,7 +15,7 @@
 					<b-navbar-nav class="ml-auto">
 						<b-nav-item to="">Home</b-nav-item>
 						<b-nav-item to="">Services</b-nav-item>
-						<b-nav-item to="">Projects</b-nav-item>
+						<b-nav-item to="/projects">Projects</b-nav-item>
 						<b-nav-item to="">Contact Us</b-nav-item>
 						<b-nav-item to="">About</b-nav-item>
 					</b-navbar-nav>
---
+++ src/components/Project.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<div class="grid-container">
+			<div class="row1">
+				<h1>Our Projects</h1>
+			</div>
+			<div class="row2">
+				<div class="project-gallery">
+					<div class="card">1</div>
+					<div class="card">2</div>
+					<div class="card">3</div>
+					<div class="card">4</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Project",
+	};
+</script>
+
+<style lang="css" scoped>
+	.grid-container {
+		display: grid;
+		grid-template-columns: auto;
+		grid-template-rows: 2fr auto;
+		grid-template-areas:
+			"title"
+			"images";
+	}
+	.row1 {
+		grid-area: title;
+		justify-self: center;
+	}
+	.row2 {
+		grid-area: images;
+		justify-self: center;
+	}
+</style>
