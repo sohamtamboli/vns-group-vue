d68854d0c53b410389370247b77ce76e0cd91d25
atharva <atharva55@yahoo.com>
2020-10-06 15:38:25 +0530

    created setting for both vue and django which work parallel in project

--- FireSafety/FireSafety/settings.py
+++ FireSafety/FireSafety/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print("BASE_DIR",BASE_DIR)
+BD=os.path.dirname(BASE_DIR)
+print("BD",os.path.dirname(BASE_DIR))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'FireSafety.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ 
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME':'dist/',
+        'STATS_FILE': os.path.join(BD,'webpack-stats.json'),
+    }
+}
--- FireSafety/FireSafety/urls.py
+++ FireSafety/FireSafety/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from contact.views import AboutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/",include('contact.api.urls'))
+    path("api/",include('contact.api.urls')),
+    path("",AboutView.as_view(),name="index")
+
 ]
 
