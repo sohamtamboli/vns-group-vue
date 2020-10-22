1828a4239aade2c405f389493ca710a26c071dba
Soham Tamboli <sohamtamboli@gmail.com>
2020-10-22 23:44:58 +0530

    added dropdown

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -16,7 +16,16 @@
 						<b-nav-item to="/">Home</b-nav-item>
 						<b-nav-item href="/#services">Services</b-nav-item>
 						<b-nav-item to="/projects">Projects</b-nav-item>
-						<b-nav-item to="/company">Company</b-nav-item>
+
+						<b-nav-item-dropdown text="Company">
+							<b-dropdown-item to="#" class="text-center"
+								>VNS Enterprises</b-dropdown-item
+							>
+							<b-dropdown-item to="#" class="text-center"
+								>VNS Solutions</b-dropdown-item
+							>
+						</b-nav-item-dropdown>
+
 						<b-nav-item to="/contact">Contact Us</b-nav-item>
 						<b-nav-item to="/about">About</b-nav-item>
 					</b-navbar-nav>
