5dcc91b21316620befe9ba15a3fb2a7b52d54d97
atharva <atharva55@yahoo.com>
2020-10-27 15:55:53 +0530

    update footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <footer>
-                <section id="footer">
+                <section id="footer" class="p-5">
                     <div class="container">
                         <div class="row text-center text-xs-center text-sm-left text-md-left">
                             <div class="col-xs-12 col-sm-4 col-md-4">
