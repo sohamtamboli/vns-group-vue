05af222b4a3ab49e8a57e849912c19445c5d1bcd
atharva <atharva55@yahoo.com>
2020-11-25 00:03:43 +0530

    update contact form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="container">
+        <form @submit.prevent="sendEmail">
+          <label>Name</label>
+          <input 
+            type="text" 
+            v-model="name"
+            name="name"
+            placeholder="Your Name"
+          >
+          <label>Email</label>
+          <input 
+            type="email" 
+            v-model="email"
+            name="email"
+            placeholder="Your Email"
+            >
+          <label>Message</label>
+          <textarea 
+            name="message"
+            v-model="message"
+            cols="30" rows="5"
+            placeholder="Message">
+          </textarea>
+          
+          <input type="submit" value="Send">
+        </form>
+    </div>
+</template>
+
+<script>
+import emailjs from 'emailjs-com';
+export default {
+  name: 'ContactUs',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        emailjs.sendForm('service_fuwzk1b', 'template_rwvoict', e.target, 'user_Mwh1a8NNuFMj0FmY1R6TR', {
+          name: this.name,
+          email: this.email,
+          message: this.meessage
+        })
+        console.log('it works!!!')
+      } catch (error) {
+          console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
 	import Carousel from "@/components/Carousel";
 	import Services from "@/components/Services";
 	import About from "@/components/AboutUs";
-	import ContactUs from "@/components/Contactus";
+	import ContactUs from "@/components/ContactForm";
 
 	export default {
 		title: "VNS Home",
@@ -27,13 +27,44 @@
 		},
 	};
 </script>
-<style lang="css" scoped>
-	#services::before {
-		display: block;
-		content: " ";
-		margin-top: -320px;
-		height: 304px;
-		visibility: hidden;
-		pointer-events: none;
-	}
+<style scoped>
+* {box-sizing: border-box;}
+
+.container {
+  display: block;
+  margin:auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 50%;
+}
+
+label {
+  float: left;
+}
+
+input[type=text], [type=email], textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
 </style>
