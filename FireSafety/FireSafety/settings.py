bf725a59fd8ad1401c3a47009897cceb870149f5
atharva <atharva55@yahoo.com>
2020-11-27 12:07:40 +0530

    git update settings

--- FireSafety/FireSafety/settings.py
+++ FireSafety/FireSafety/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "vnsgroupofficial@gmail.com"
-EMAIL_HOST_PASSWORD = "VnsGroup@123"
+EMAIL_HOST_USER = "<****>"
+EMAIL_HOST_PASSWORD = "<*>"
 
 
 
